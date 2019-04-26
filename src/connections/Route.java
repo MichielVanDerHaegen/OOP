7e03a8fb17f1d89d88b554aa8207b92408f9c702
MichielVanDerHaegen <michiel.vdhaegen@gmail.com>
2019-04-26 12:03:48 +0200

    Update Route.java
    changes

--- src/connections/Route.java
+++ src/connections/Route.java
@@ -21,48 +21,55 @@ public class Route {
 	}
 
 	public boolean areValidSegments(Road...roads) {
-		int length = roads.length;
-		if (length == 0)
+		Location startLocation=this.startLocation;
+		if (roads.length == 0)
 			return true;
 		Road firstSegment=roads[0];
-		Road lastSegment=roads[length-1];
-		//if the startlocation is equal to one of the locations of the first segment
-		if(firstSegment.getEndPoint1()==this.startLocation || firstSegment.getEndPoint2()==this.startLocation) {
-			//then check that the other location of the firstsegment is equal to one of the locations of the second segment
-			if (length == 1)
+		if(firstSegment.getEndPoint1()==startLocation || firstSegment.getEndPoint2()==startLocation) {
+			if (roads.length == 1)
 					return true;
-			if(getOtherLocation(firstSegment)==roads[1].getEndPoint1() || getOtherLocation(firstSegment)==roads[1].getEndPoint2()) {
-				//then for each segment, check that one of its locations is equal to one of the locations of the previous one
-				//and equal to one of the locations of the next one
-				for(int i=2;i<roads.length-1;i++) {
-					if (roads[i].getEndPoint1() == roads[i - 1].getEndPoint1()
-							|| roads[i].getEndPoint1() == roads[i - 1].getEndPoint2()) {
-						if (roads[i].getEndPoint2() == roads[i + 1].getEndPoint1()
-								|| roads[i].getEndPoint2() == roads[i + 1].getEndPoint2()) {
-						}
-					}
-					if (roads[i].getEndPoint2() == roads[i - 1].getEndPoint1()
-							|| roads[i].getEndPoint2() == roads[i - 1].getEndPoint2()) {
-						if (roads[i].getEndPoint1() == roads[i + 1].getEndPoint1()
-								|| roads[i].getEndPoint1() == roads[i + 1].getEndPoint2()) {
-						}
-					}
-				}
-//					assert (roads[i].getEndPoint1()==roads[i-1].getEndPoint1()) || (roads[i].getEndPoint1()==roads[i-1].getEndPoint2()) || (roads[i].getEndPoint2()==roads[i-1].getEndPoint1()) || (roads[i].getEndPoint2()==roads[i-1].getEndPoint2());
-//					assert (roads[i].getEndPoint1()==roads[i+1].getEndPoint1()) || (roads[i].getEndPoint1()==roads[i+1].getEndPoint2()) || (roads[i].getEndPoint2()==roads[i+1].getEndPoint1()) || (roads[i].getEndPoint2()==roads[i+1].getEndPoint2());
-				
-				//finally, check that the second to last segment has one location in common with the last segment
-				assert (roads[length-2].getEndPoint1()==lastSegment.getEndPoint1()) || (roads[length-2].getEndPoint1()==lastSegment.getEndPoint2()) || (roads[length-2].getEndPoint2()==lastSegment.getEndPoint1()) || (roads[length-2].getEndPoint2()==lastSegment.getEndPoint2());
-				return true;
-			}
-			return false;
 		}
-		return false;
-	}
+		for(int i=0;i<roads.length-2;i++) {
+			if(roads[i].getEndPoint1()==startLocation || roads[i].getEndPoint2()==startLocation) {
+				startLocation=getOtherLocation(roads[i],startLocation);
+			}
+			else
+				return false;
+		}
+		return true;
+	
+//			if(getOtherLocation(firstSegment)==roads[1].getEndPoint1() || getOtherLocation(firstSegment)==roads[1].getEndPoint2()) {
+//				//then for each segment, check that one of its locations is equal to one of the locations of the previous one
+//				//and equal to one of the locations of the next one
+//				for(int i=2;i<roads.length-1;i++) {
+//					if (roads[i].getEndPoint1() == roads[i - 1].getEndPoint1()
+//							|| roads[i].getEndPoint1() == roads[i - 1].getEndPoint2()) {
+//						if (roads[i].getEndPoint2() == roads[i + 1].getEndPoint1()
+//								|| roads[i].getEndPoint2() == roads[i + 1].getEndPoint2()) {
+//						}
+//					}
+//					if (roads[i].getEndPoint2() == roads[i - 1].getEndPoint1()
+//							|| roads[i].getEndPoint2() == roads[i - 1].getEndPoint2()) {
+//						if (roads[i].getEndPoint1() == roads[i + 1].getEndPoint1()
+//								|| roads[i].getEndPoint1() == roads[i + 1].getEndPoint2()) {
+//						}
+//					}
+//				}
+////					assert (roads[i].getEndPoint1()==roads[i-1].getEndPoint1()) || (roads[i].getEndPoint1()==roads[i-1].getEndPoint2()) || (roads[i].getEndPoint2()==roads[i-1].getEndPoint1()) || (roads[i].getEndPoint2()==roads[i-1].getEndPoint2());
+////					assert (roads[i].getEndPoint1()==roads[i+1].getEndPoint1()) || (roads[i].getEndPoint1()==roads[i+1].getEndPoint2()) || (roads[i].getEndPoint2()==roads[i+1].getEndPoint1()) || (roads[i].getEndPoint2()==roads[i+1].getEndPoint2());
+//				
+//				//finally, check that the second to last segment has one location in common with the last segment
+//				assert (roads[length-2].getEndPoint1()==lastSegment.getEndPoint1()) || (roads[length-2].getEndPoint1()==lastSegment.getEndPoint2()) || (roads[length-2].getEndPoint2()==lastSegment.getEndPoint1()) || (roads[length-2].getEndPoint2()==lastSegment.getEndPoint2());
+//				return true;
+//			}
+//			return false;
+//		}
+//		return false;
+		}
 	
 	
-	public Location getOtherLocation(Road road) {
-		if(road.getEndPoint1()==this.startLocation)
+	public Location getOtherLocation(Road road, Location startLocation) {
+		if(road.getEndPoint1()==startLocation)
 			return road.getEndPoint2();
 		else
 			return road.getEndPoint1();
