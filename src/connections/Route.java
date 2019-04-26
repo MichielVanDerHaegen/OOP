88fe8907fb18ae39bc2bded88b642b83e53a509e
MichielVanDerHaegen <michiel.vdhaegen@gmail.com>
2019-04-26 12:24:13 +0200

    Update Route.java

--- src/connections/Route.java
+++ src/connections/Route.java
@@ -5,118 +5,141 @@ import java.util.Arrays;
 
 public class Route {
 
+	/**
+	 * 
+	 */
 	private final Location startLocation;
+	
+	/**
+	 * 
+	 */
 	private Road[] roadSegments;
 
-	//private final Location endLocation;
-
-
-	public Route(Location startLocation, Road...roads) throws IllegalArgumentException, NullPointerException {
+	// private final Location endLocation;
+	
+	/**
+	 * 
+	 * @param startLocation
+	 * @param roads
+	 * @throws IllegalArgumentException
+	 * @throws NullPointerException
+	 */
+	public Route(Location startLocation, Road... roads) throws IllegalArgumentException, NullPointerException {
 		if (startLocation == null)
 			throw new NullPointerException();
-		this.startLocation=startLocation;
-		if(!areValidSegments(roads))
+		this.startLocation = startLocation;
+		if (!areValidSegments(roads))
 			throw new IllegalArgumentException();
 		roadSegments = roads.clone();
 	}
 
-	public boolean areValidSegments(Road...roads) {
-		Location startLocation=this.startLocation;
+	/**
+	 * 
+	 * @param roads
+	 * @return
+	 */
+	public boolean areValidSegments(Road... roads) {
+		Location startLocation = this.startLocation;
 		if (roads.length == 0)
 			return true;
-		Road firstSegment=roads[0];
-		if(firstSegment.getEndPoint1()==startLocation || firstSegment.getEndPoint2()==startLocation) {
+		if (roads[0].getEndPoint1() == startLocation || roads[0].getEndPoint2() == startLocation) {
 			if (roads.length == 1)
-					return true;
+				return true;
+			startLocation = getOtherLocation(roads[0], startLocation);
+			for (int i = 1; i <= roads.length - 1; i++) {
+				assert (roads[i].getEndPoint1() == startLocation || roads[i].getEndPoint2() == startLocation);
+				startLocation = getOtherLocation(roads[i], startLocation);
+			}
+			return true;
 		}
-		else
-			return false;
-		for(int i=0;i<roads.length-1;i++) {
-			assert (roads[i].getEndPoint1()==startLocation || roads[i].getEndPoint2()==startLocation);
-			startLocation=getOtherLocation(roads[i],startLocation);
-		}
-		return true;
-	
-//			if(getOtherLocation(firstSegment)==roads[1].getEndPoint1() || getOtherLocation(firstSegment)==roads[1].getEndPoint2()) {
-//				//then for each segment, check that one of its locations is equal to one of the locations of the previous one
-//				//and equal to one of the locations of the next one
-//				for(int i=2;i<roads.length-1;i++) {
-//					if (roads[i].getEndPoint1() == roads[i - 1].getEndPoint1()
-//							|| roads[i].getEndPoint1() == roads[i - 1].getEndPoint2()) {
-//						if (roads[i].getEndPoint2() == roads[i + 1].getEndPoint1()
-//								|| roads[i].getEndPoint2() == roads[i + 1].getEndPoint2()) {
-//						}
-//					}
-//					if (roads[i].getEndPoint2() == roads[i - 1].getEndPoint1()
-//							|| roads[i].getEndPoint2() == roads[i - 1].getEndPoint2()) {
-//						if (roads[i].getEndPoint1() == roads[i + 1].getEndPoint1()
-//								|| roads[i].getEndPoint1() == roads[i + 1].getEndPoint2()) {
-//						}
-//					}
-//				}
-////					assert (roads[i].getEndPoint1()==roads[i-1].getEndPoint1()) || (roads[i].getEndPoint1()==roads[i-1].getEndPoint2()) || (roads[i].getEndPoint2()==roads[i-1].getEndPoint1()) || (roads[i].getEndPoint2()==roads[i-1].getEndPoint2());
-////					assert (roads[i].getEndPoint1()==roads[i+1].getEndPoint1()) || (roads[i].getEndPoint1()==roads[i+1].getEndPoint2()) || (roads[i].getEndPoint2()==roads[i+1].getEndPoint1()) || (roads[i].getEndPoint2()==roads[i+1].getEndPoint2());
-//				
-//				//finally, check that the second to last segment has one location in common with the last segment
-//				assert (roads[length-2].getEndPoint1()==lastSegment.getEndPoint1()) || (roads[length-2].getEndPoint1()==lastSegment.getEndPoint2()) || (roads[length-2].getEndPoint2()==lastSegment.getEndPoint1()) || (roads[length-2].getEndPoint2()==lastSegment.getEndPoint2());
-//				return true;
-//			}
-//			return false;
-//		}
-//		return false;
-		}
-	
-	
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param road
+	 * @param startLocation
+	 * @return
+	 */
 	public Location getOtherLocation(Road road, Location startLocation) {
-		if(road.getEndPoint1()==startLocation)
+		if (road.getEndPoint1() == startLocation)
 			return road.getEndPoint2();
 		else
 			return road.getEndPoint1();
-		
+
 	}
 
-	public Location getStartLocation(){
+	/**
+	 * 
+	 * @return
+	 */
+	public Location getStartLocation() {
 		return startLocation;
 	}
 
-	public Road[] getRouteSegements(){
+	/**
+	 * 
+	 * @return
+	 */
+	public Road[] getRouteSegements() {
 		return roadSegments.clone();
 	}
-	
-	
-	public void addRouteSegment(Road road) throws IllegalArgumentException{
-		if(road==null)
+
+	/**
+	 * 
+	 * @param road
+	 * @throws IllegalArgumentException
+	 */
+	public void addRouteSegment(Road road) throws IllegalArgumentException {
+		if (road == null)
 			throw new IllegalArgumentException();
 		ArrayList<Road> list = new ArrayList<Road>(Arrays.asList(roadSegments));
 		list.add(road);
-		roadSegments=list.toArray(roadSegments);
-		assert(areValidSegments(roadSegments));
+		roadSegments = list.toArray(roadSegments);
+		assert (areValidSegments(roadSegments));
 	}
-	
-	public void removeRouteSegment(int index) throws IllegalArgumentException{
-		if(index < 0 || index>=roadSegments.length)
+
+	/**
+	 * 
+	 * @param index
+	 * @throws IllegalArgumentException
+	 */
+	public void removeRouteSegment(int index) throws IllegalArgumentException {
+		if (index < 0 || index >= roadSegments.length)
 			throw new IllegalArgumentException();
 		ArrayList<Road> list = new ArrayList<Road>(Arrays.asList(roadSegments));
 		list.remove(index);
-		roadSegments=list.toArray(roadSegments);
-		assert(areValidSegments(roadSegments));
+		roadSegments = list.toArray(roadSegments);
+		assert (areValidSegments(roadSegments));
 	}
-	
+
+	/**
+	 * 
+	 * @return
+	 */
 	public int getTotalLength() {
 		int length = 0;
-		for(Road road: roadSegments) {
+		for (Road road : roadSegments) {
 			length += road.getLength();
 		}
 		return length;
 	}
-	
+
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean isTraversable() {
 		return false;
 	}
-	
+
+	/**
+	 * 
+	 * @return
+	 */
 	public Location[] getAllLocations() {
 		ArrayList<Location> list = new ArrayList<Location>();
-		for(Road road: roadSegments) {
+		for (Road road : roadSegments) {
 			list.add(road.getEndPoint1());
 			list.add(road.getEndPoint2());
 		}
